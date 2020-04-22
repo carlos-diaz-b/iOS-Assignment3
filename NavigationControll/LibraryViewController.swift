4044b1b059b3fd9d1762319cc4f5b2ca5b451b64
Carlos Diaz <carlosandresdiazbravo@Carloss-MacBook-Pro.local>
2020-04-22 14:24:20 -0700

    ok

---
+++ NavigationControll/LibraryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LibraryViewController.swift
+//  NavigationControll
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-22.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+
+}
---
+++ NavigationControll/PorchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PorchViewController.swift
+//  NavigationControll
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-22.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+import UIKit
+
+class PorchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
